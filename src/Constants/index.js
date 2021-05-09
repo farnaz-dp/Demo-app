93401548dcb3913f77eccb3c32eba51450bcddac
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 15:57:52 +0430

    index.js constants added to Git

---
+++ src/Constants/index.js
@@ -0,0 +1,29 @@
+export {
+    LOGIN_API_REQUEST ,
+    LOGOUT_USER ,
+    LOGIN_SET_USER ,
+    LOGIN_API_SUCCESS ,
+    LOGIN_API_ERROR
+} from './LoginApi/LoginConstants'
+
+export {
+    MAPDATA_API_ERROR,
+    MAPDATA_API_SUCCESS,
+    MAPDATA_API_REQUEST
+} from './MapData/MapConstants'
+
+export {
+    SVG_CLICK,
+    SVG_UNLOADED,
+    SVG_LOADED
+} from './SVG/SvgShow'
+
+export {
+    CONTENT_NOT_SHOW,
+    CONTENT_SHOW
+} from './Content/ContentConstant'
+
+export {
+    DRAWER_NOT_SHOW,
+    DRAWER_SHOW
+} from './Drawer/DrawerConstant'
