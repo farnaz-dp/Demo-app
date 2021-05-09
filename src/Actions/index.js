74d69f5d88f47d0bdcc40642eae883d6ecee64a1
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 15:57:32 +0430

    index.js action added to Git

---
+++ src/Actions/index.js
@@ -0,0 +1,33 @@
+export {
+    LogoutUser,
+    LoginApiSuccess,
+    LoginApiError,
+    LoginApiRequest,
+    fetchToken,
+    LoginSetUSer
+} from './LoginApi/LoginActions'
+
+export {
+    fetchMapData,
+    MapDataApiError,
+    MapDataApiSuccess,
+    MapDataApiRequest
+
+} from './MapData/MapDataAction'
+
+export {
+    SvgClick,
+    SvgLoaded,
+    SvgUnLoaded
+} from './SVG/SvgAction'
+
+
+export {
+    ContentShow,
+    ContentNotShow
+} from './ContentAction/ContentAction'
+
+export {
+    DrawerNotShow,
+    DrawerShow
+} from './DrawerAction/DrawerAction'
