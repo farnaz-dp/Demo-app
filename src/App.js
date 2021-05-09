dee1eaeb0690e9597131bef11f8619161b8c7b3d
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 15:50:16 +0430

    App.js added to Git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux"
+import {MainApp} from './MainApp'
+;import {store} from './Store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Provider store={store}>
+         <BrowserRouter>
+             <MainApp/>
+         </BrowserRouter>
+     </Provider>
+
+
   );
 }
 
