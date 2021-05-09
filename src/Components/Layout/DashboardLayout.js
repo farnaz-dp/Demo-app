75577b26bba9691bfa1d70ab0dac1840f941b1ac
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 15:54:47 +0430

    DashboardLayout.js added to Git

---
+++ src/Components/Layout/DashboardLayout.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import {Dashboard} from "../Dashboard";
+import {AppRoute} from "../../Route/AppRoute";
+
+const DashboardLayout = (props) => {
+    return (
+        <div>
+            <Dashboard/>
+
+            {/*<AppRoute/>*/}
+        </div>
+    )
+}
+
+
+export {DashboardLayout}
