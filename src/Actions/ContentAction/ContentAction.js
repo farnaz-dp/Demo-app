6342fbaa87f17bacce5ca346de7ba9d18eb24809
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 15:52:25 +0430

    ContentAction.js added to Git

---
+++ src/Actions/ContentAction/ContentAction.js
@@ -0,0 +1,14 @@
+
+import {CONTENT_SHOW , CONTENT_NOT_SHOW} from '../../Constants'
+
+export const ContentShow = () => {
+    return {
+        type : CONTENT_SHOW
+    }
+}
+
+export const ContentNotShow = () => {
+    return {
+        type : CONTENT_NOT_SHOW
+    }
+}
