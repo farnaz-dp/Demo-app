6b835fcdf693a2759a28ae1abe791e19c2a1e9d0
farnaz <farnaz.dp7394@gmail.com>
2021-05-09 16:00:17 +0430

    LoginLayout.js added to Git

---
+++ src/Components/Layout/LoginLayout.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import {AppRoute} from "../../Route/AppRoute";
+
+const LoginLayout = () => {
+    return(
+        <div>
+            <AppRoute/>
+        </div>
+    )
+}
+
+export {LoginLayout}
